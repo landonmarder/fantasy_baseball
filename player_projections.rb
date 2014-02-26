1589e846f9050952b230d1dec2733d0100e9b09a
landonmarder <landon.marder@gmail.com>
2014-02-26 14:37:31 -0500

    refactor to find position

--- player_projections.rb
+++ player_projections.rb
@@ -2,23 +2,35 @@ require 'rest_client'
 require 'json'
 require 'pry'
 
-POSITIONS = %w{1B 2B SS C 3B OF DH}
+class PlayerProjections
 
-response_batters = RestClient.get 'http://www.kimonolabs.com/api/bt868shs?apikey=455e95d967d14e53ad7188d10746bcf6'
+  POSITIONS = %w{1B 2B SS C 3B OF DH}
 
-json_batters = JSON.parse(response_batters)
+  def initialize
+    response_batters = RestClient.get 'http://www.kimonolabs.com/api/bt868shs?apikey=455e95d967d14e53ad7188d10746bcf6'
+    json_batters = JSON.parse(response_batters)
+    @batters = json_batters['results']['collection1']
+  end
 
-batters = json_batters['results']['collection1']
+  def parse_batters
+    @batters.each do |batter|
+      player_info = batter['name']['text']
+      name = player_info.split('.')[-1].split(',')[0][1..-1]
+      positions = []
 
-batters.each do |batter|
-  player_info = batter['name']['text']
-  name = player_info.split('.')[-1].split(',')[0][1..-1]
-  positions = []
+      find_position(positions, player_info)
+      binding.pry
+    end
+  end
 
-  POSITIONS.each do |position|
-    positions << position if player_info.gsub(',','').split(' ').include?(position)
+
+  def find_position(arr, player)
+    POSITIONS.map { |position|
+      arr << position if player.gsub(',','').split(' ').include?(position)
+    }
   end
 end
 
+PlayerProjections.new().parse_batters
 
 # binding.pry
