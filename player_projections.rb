a9e3a376187810ae8edcc297caf41a12dc6de7da
landonmarder <landon.marder@gmail.com>
2014-02-26 14:24:42 -0500

    add multi positions for each player

--- player_projections.rb
+++ player_projections.rb
@@ -2,16 +2,22 @@ require 'rest_client'
 require 'json'
 require 'pry'
 
+POSITIONS = %w{1B 2B SS C 3B OF DH}
+
 response_batters = RestClient.get 'http://www.kimonolabs.com/api/bt868shs?apikey=455e95d967d14e53ad7188d10746bcf6'
 
 json_batters = JSON.parse(response_batters)
 
 batters = json_batters['results']['collection1']
 
-arr = []
-
 batters.each do |batter|
-  name = batter['name']['text'].split('.')[-1].split(',')[0][1..-1]
+  player_info = batter['name']['text']
+  name = player_info.split('.')[-1].split(',')[0][1..-1]
+  positions = []
+
+  POSITIONS.each do |position|
+    positions << position if player_info.gsub(',','').split(' ').include?(position)
+  end
 end
 
 
